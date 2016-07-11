0c5058bef1dd3e722c895b01060adf3e613b1e46
marinofelipe <felipemarino91@gmail.com>
2016-07-10 23:40:21 -0300

    Hobby Share project branch creation commit

---
+++ hoBshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hoBshare
+//
+//  Created by Felipe Lefevre Marino on 10/07/16.
+//  Copyright © 2016 Felipe Lefevre Marino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
