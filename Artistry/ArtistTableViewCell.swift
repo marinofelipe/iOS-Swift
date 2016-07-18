5e505525f3346392a15d7be7dcc2b195451e27a8
resource <resource@Mac-mini-de-resource.local>
2016-07-18 16:36:48 -0300

    self-sizing table view cells study app

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by resource on 7/18/16.
+//  Copyright © 2016 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet var bioLabel: UILabel!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var artistImageView: UIImageView!
+
+}
