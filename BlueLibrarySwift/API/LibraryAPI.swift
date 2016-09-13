59344966418d6edde92066a2fd28a4288f7ed049
Felipe Lefevre Marino <felipemarino91@gmail.com>
2016-09-13 17:10:12 -0300

    Facade Pattern on LibraryAPI class

--- BlueLibrarySwift/API/LibraryAPI.swift
+++ BlueLibrarySwift/API/LibraryAPI.swift
@@ -13,10 +13,41 @@ class LibraryAPI: NSObject {
     class var sharedInstance: LibraryAPI {
         
         struct Singleton {
-            
             static let instance = LibraryAPI()
         }
         
         return Singleton.instance
     }
+    
+    private let persistenceManager: PersistencyManager
+    private let httpClient: HTTPClient
+    private let isOnline: Bool
+    
+    
+    override init() {
+        persistenceManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
+    
+    func getAlbums() -> [Album] {
+        return persistenceManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int) {
+        persistenceManager.addAlbum(album, index: index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistenceManager.deleteAlbumAtIndex(index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+    
 }
